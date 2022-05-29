b1d6f5969422668f3b101f476e722df93b3e1696
Donkrzawayan <kadon991@gmail.com>
2022-05-29 20:51:06 +0200

    widgets placement and displaying uploaded video

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+
 from tkvideo import tkvideo
 
 
@@ -6,15 +8,38 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # set window title
-        self.title('Video Player')
+        self.title('OCMotDetVal')
+
+        self._set_frame(0, 0)
+        self._set_frame(1, 0)
+        self._set_frame(1, 1)
+        self._set_frame(1, 2)
+
+        self.upload_button = tk.Button(self, text='Open',
+                                       command=lambda: self._upload_action())
+        self.upload_button.grid(row=0, column=2)
+
+    def _set_frame(self, row, column):
+        self.frame = tk.Frame(self, width=350, height=250, background='darkgrey')
+        self.frame.grid(row=row, column=column)
+
+    def _upload_action(self, event=None):
+        self.filename = filedialog.askopenfilename()
+
+        if self.filename == '':
+            return
+
+        try:
+            self.upload_label.grid_forget()
+        except AttributeError:
+            pass
+
         # create label
-        self.video_label = tk.Label(self)
-        self.video_label.pack()
+        self.upload_label = tk.Label(self)
+        self.upload_label.grid(row=0, column=0)
         # read video to display on label
-        self.player = tkvideo("path/to/video.mp4", self.video_label,
-                              loop=1, size=(700, 500))
-        self.player.play()
+        player = tkvideo(self.filename, self.upload_label, loop=1, size=(350, 250))
+        player.play()
 
 
 if __name__ == "__main__":
